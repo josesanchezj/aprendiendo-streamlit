61af9d6402a46c6cf1796fe7925876340d9a8ff5
josesanchezj <118290971+josesanchezj@users.noreply.github.com>
2024-05-29 17:54:11 +0200

    Update chatio.py

--- chatio.py
+++ chatio.py
@@ -6,7 +6,7 @@ st.title("ChatGPT-like clone")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -15,7 +15,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Hola, que tal?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
